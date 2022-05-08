5cada7fec610166a9f81c2dd482928d44a1cef60
kfonseca <kfonseca.hp@gmail.com>
2022-05-08 04:56:18 -0600

    Update streamlit_app.py
    Displayed information from fruityvice successfully.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ streamlit.dataframe(fruits_to_show)
 # Additional reference: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
 
 # New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
